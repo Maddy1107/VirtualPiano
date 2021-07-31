63fb078f0b33c0c9d4ae49e03e708841e98efcfe
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2021-07-31 19:52:55 +0100

    Initial Change

---
+++ VPiano/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class AudioManager
+{
+    /*public static void PlaySound()
+    {
+        GameObject aud = new GameObject("Sound");
+        AudioSource auds = aud.AddComponent<AudioSource>();
+        auds.PlayOneShot(CubeScript._i.C);
+    }*/
+}
---
+++ VPiano/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    bool isPressed = false;
+    Material mat;
+    public AudioSource C;
+    public AudioClip C1;
+
+    private void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        C = GetComponent<AudioSource>();
+    }
+    public void Pressed()
+    {
+        isPressed = true;
+    }
+
+    public void Released()
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed)
+        {
+            mat.color = Color.cyan;
+            C.PlayOneShot(C1);
+        }
+        else
+        {
+            mat.color = Color.magenta;
+            //aud.Stop();
+        }
+    }
+}
