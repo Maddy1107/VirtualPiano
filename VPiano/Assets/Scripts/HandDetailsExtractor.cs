723aa2e343bf74513abfa8780ed9f242842f992a
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2021-08-16 22:23:39 +0100

    WW

---
+++ VPiano/Assets/Scripts/HandDetailsExtractor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class HandDetailsExtractor : MonoBehaviour
+{
+    public static HandDetailsExtractor instance;
+
+    Hand hand;
+
+    Controller controller;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Update()
+    {
+        hand = GetComponent<CapsuleHand>().GetLeapHand(); ;
+    }
+
+    public Vector3 GetHandPos()
+    {
+        return hand.PalmPosition.ToVector3();
+    }
+
+    /*public bool GetHandLeft()
+    {
+        return hand.IsLeft;
+    }
+
+    public bool GetHandRight()
+    {
+        return hand.IsRight;
+    }*/
+
+}
---
+++ VPiano/Assets/Scripts/ShiftView.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class ShiftView : MonoBehaviour
+{
+    float LeapRigEndLeft;
+    float LeapRigEndRight;
+
+    float CameraEndLeft = 26f;
+    float CameraEndRight = 45f;
+
+    public GameObject LeapRigObj;
+
+    Vector3 LeapRigTarget;
+    Vector3 CameraTarget;
+
+    float smoothness = 2f;
+
+    private void Start()
+    {
+        LeapRigTarget = LeapRigObj.transform.position;
+        CameraTarget = Camera.main.transform.position;
+    }
+
+    private void Update()
+    {
+        float LeapRigEndLeft = LeapRigObj.transform.position.x - (LeapRigObj.transform.localScale.x / 2);
+        float LeapRigEndRight = LeapRigObj.transform.position.x + (LeapRigObj.transform.localScale.x / 2);
+        float HandXPos = HandDetailsExtractor.instance.GetHandPos().x;
+
+        if (HandXPos <= LeapRigEndLeft)
+        { ViewShift("Left"); }
+        else if (HandXPos >= LeapRigEndRight)
+        { ViewShift("Right"); }
+
+        Shift();
+        Debug.Log("Hand PosX = " + HandXPos);
+        Debug.Log("LeftEnd = " + LeapRigEndLeft);
+        Debug.Log("RightEnd = " + LeapRigEndRight);
+    }
+
+    public void Shift()
+    {
+        LeapRigObj.transform.position = Vector3.Lerp(LeapRigObj.transform.position, LeapRigTarget, smoothness * Time.deltaTime);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, CameraTarget, smoothness * Time.deltaTime);
+    }
+
+    public void ViewShift(string direction)
+    {
+        if (direction == "Left")
+        {
+            if (Camera.main.transform.position.x < CameraEndLeft)
+                return;
+
+            LeapRigTarget.x = LeapRigEndLeft;
+            CameraTarget.x = CameraEndLeft;
+        }
+        else if (direction == "Right")
+        {
+            if (Camera.main.transform.position.x < CameraEndRight)
+                return;
+
+            LeapRigTarget.x = LeapRigEndRight;
+            CameraTarget.x = CameraEndRight;
+        }
+    }
+}
